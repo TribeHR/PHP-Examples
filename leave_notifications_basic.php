beaecda1e5a4f5ddcdd531b117d5ed6e105e5ffc
Derek Hall <einhander.iniq@gmail.com>
2013-09-03 09:06:59 -0400

    Improve comments and context of the example

--- leave_notifications_basic.php
+++ leave_notifications_basic.php
@@ -1,6 +1,15 @@
 <?php
-// key: requesting user's ID
-// value: the email address to notify
+/**
+ * A very basic example of what you can do using TribeHR webhooks without the API.
+ * This uses a hard-coded array to simply send a basic email when specific TribeHR Users
+ * add a new LeaveRequest.
+ * To see how to get more information about the LeaveRequest or the adding User, look to 
+ * more advanced examples that connect to the TribeHR API.
+ */
+
+// Create one array entry per notification rule you wish to create:
+// key: adding user's ID
+// value: the email address(es) to notify when the adding user creates a LeaveRequest - either a string or an array
 $leaveApprovers = array(
 	12 => 'supervisorperson1@mycompany.com',
 	3 => 'supervisorperson1@mycompany.com',
